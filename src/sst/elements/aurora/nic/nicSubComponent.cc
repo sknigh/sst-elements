e0a33f7eab3d514d040384e4d7229b52e58b7ff8
Levenhagen <mjleven@s1043496.srn.sandia.gov>
2019-09-30 10:38:32 -0600

    move common code into NicSubComponent base class

---
+++ src/sst/elements/aurora/nic/nicSubComponent.cc
@@ -0,0 +1,57 @@
+// Copyright 2009-2018 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2018, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#include "sst_config.h"
+
+#include "nic/nic.h"
+#include "nic/nicSubComponent.h"
+
+using namespace SST;
+using namespace SST::Aurora;
+
+NicSubComponent::NicSubComponent( ComponentId_t id, Params params ) : SubComponent(id),  m_clocking(false), m_totalIdleCycles(0) {
+
+    UnitAlgebra    m_clockRate = params.find<UnitAlgebra>("clock","100Mhz");
+    m_dbg.verbose(CALL_INFO,1,1,"clockRate=%s\n", m_clockRate.toString().c_str());
+
+	m_toHostLatency = params.find<SimTime_t>("toHostLatency",0);
+    m_selfLink = configureSelfLink("Nic::selfLink", "1 ns",
+       new Event::Handler<NicSubComponent>(this,&NicSubComponent::handleSelfEvent));
+    assert( m_selfLink );
+
+    if ( m_clockRate.getRoundedValue() ) {
+        m_clockHandler = new Clock::Handler<NicSubComponent>(this,&NicSubComponent::clockHandler);
+         m_timeConverter = registerClock( m_clockRate, m_clockHandler );
+        m_clocking = true;
+     }
+}
+
+void NicSubComponent::stopClocking( Cycle_t cycle ) {
+	assert( m_clocking == true );
+
+	m_dbg.debug(CALL_INFO,1,2,"stop clocking cycle %" PRIu64 "\n",cycle);
+	m_nic->enableNetNotifier();
+	m_clocking = false;
+	m_stopCycle = cycle;
+}
+
+void NicSubComponent::startClocking() {
+
+	m_dbg.debug(CALL_INFO,1,2,"\n");
+	m_nic->disableNetNotifier();
+	_startClocking();
+	m_totalIdleCycles += getNextClockCycle(m_timeConverter) - m_stopCycle;
+}
--- src/sst/elements/aurora/nic/nicSubComponent.h
+++ src/sst/elements/aurora/nic/nicSubComponent.h
@@ -19,11 +19,13 @@
 #include <sst/core/subcomponent.h>
 #include <sst/core/link.h>
 #include <sst/core/interfaces/simpleNetwork.h>
+#include <sst/core/params.h>
 #include "include/nicEvents.h"
 
 namespace SST {
 namespace Aurora {
 
+class Nic;
 class NicSubComponent : public SubComponent {
 
   public:
@@ -31,38 +33,73 @@ class NicSubComponent : public SubComponent {
     SST_ELI_REGISTER_SUBCOMPONENT_API(SST::Aurora::NicSubComponent)
 
     NicSubComponent( Component* owner ) : SubComponent(owner) {assert(0);}
-    NicSubComponent( ComponentId_t id ) : SubComponent(id) {}
-    virtual void setup() {}
-    virtual void finish() {}
+    NicSubComponent( ComponentId_t, Params );
 
-	virtual void init() {} 
+    virtual void setup() {}
+    virtual void finish() {
+#if 0
+		Cycle_t cycle  = getNextClockCycle(m_timeConverter);
+		printf("total cycles %" PRIu64 ", idle cycles %" PRIu64 " idle %.1f%%\n", cycle, m_totalIdleCycles,  ((double) m_totalIdleCycles / cycle ) * 100);
+#endif
+	}
+
+	virtual void init( Nic* nic ) { m_nic = nic; } 
 	void setNetworkLink( Interfaces::SimpleNetwork* link ) { m_netLink= link; } 
 	virtual void setNumCores( int num ) { m_toCoreLinks.resize(num); }
 	void setCoreLink( int core, Link* link ) { m_toCoreLinks[core] = link; }
 	int getNumCores() { return m_toCoreLinks.size(); }
 	void setPktSize( int size ) { m_pktSize = size; } 
 
-
 	Interfaces::SimpleNetwork& getNetworkLink( ) { return *m_netLink; }
 	void sendResp( int core, Event* event ) {
-		m_toCoreLinks[core]->send(0,new NicEvent( event) ); 
+		m_toCoreLinks[core]->send(m_toHostLatency,new NicEvent( event) ); 
 	}
 
 	virtual void setNodeNum( int num ) { m_nodeNum = num; }
 	virtual int getNodeNum( ) { return m_nodeNum; }
 	virtual void handleEvent( int core, Event* ) = 0;
 	virtual bool clockHandler( Cycle_t ) { assert(0); }
+	virtual void handleSelfEvent( Event* ) { assert(0); }
 	static int getPktSize() { return m_pktSize; }
 
-	virtual bool recvNotify( int vc ) { assert(0); } 
-	virtual bool sendNotify( int vc ) { assert(0); } 
-	virtual void setRecvNotify( std::function<void()> ) { assert(0); } 
-	virtual void setSendNotify( std::function<void()> ) { assert(0); } 
+    void networkReady( int vc ) {
+        m_dbg.debug(CALL_INFO,1,2,"\n");
+        _startClocking();
+    }
+
+  protected:
+
+    void stopClocking( Cycle_t cycle );
+    void startClocking();
+    void _startClocking() {
+
+        if ( m_clocking != false ) {
+            m_dbg.debug(CALL_INFO,1,2,"\n");
+            assert(0);
+        }
+        m_clocking = true;
+        Cycle_t cycle = reregisterClock( m_timeConverter, m_clockHandler );
+        m_dbg.debug(CALL_INFO,1,2,"start clocking cycle %" PRIu64 "\n",cycle);
+    }
+
+	TimeConverter* m_timeConverter;	
+
+	Link*	m_selfLink;
+	bool	m_clocking;
+	Output	m_dbg;
 
   private:
-    std::vector<Link*>          m_toCoreLinks;
-	Interfaces::SimpleNetwork*  m_netLink;
-	int m_nodeNum;
+    std::vector<Link*>          		m_toCoreLinks;
+	Clock::Handler<NicSubComponent>*	m_clockHandler;
+	Interfaces::SimpleNetwork*  		m_netLink;
+
+	int 	m_nodeNum;
+	Nic* 	m_nic;
+
+	SimTime_t m_toHostLatency;	
+	Cycle_t m_totalIdleCycles;
+	Cycle_t m_stopCycle;
+
 	static int m_pktSize;
 };
 
