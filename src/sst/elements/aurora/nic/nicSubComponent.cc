e461656134aea90e7f2ea05c9d342686e73476df
Levenhagen <mjleven@s1043496.srn.sandia.gov>
2019-10-25 15:35:30 -0600

    fix white space and debug

--- src/sst/elements/aurora/nic/nicSubComponent.cc
+++ src/sst/elements/aurora/nic/nicSubComponent.cc
@@ -22,35 +22,36 @@
 using namespace SST;
 using namespace SST::Aurora;
 
-NicSubComponent::NicSubComponent( ComponentId_t id, Params params ) : SubComponent(id),  m_clocking(false), m_totalIdleCycles(0) {
+NicSubComponent::NicSubComponent( ComponentId_t id, Params params ) : SubComponent(id),  m_clocking(false), m_totalIdleCycles(0), m_vc(0) {
 
     UnitAlgebra    m_clockRate = params.find<UnitAlgebra>("clock","100Mhz");
-    m_dbg.verbose(CALL_INFO,1,1,"clockRate=%s\n", m_clockRate.toString().c_str());
+	SST::UnitAlgebra busBandwidth = params.find<SST::UnitAlgebra>( "busBandwidth", "15GB/s" );
 
 	m_toHostLatency = params.find<SimTime_t>("toHostLatency",0);
 	m_rxLatency = params.find<SimTime_t>("rxLatency",0);
 	m_txLatency = params.find<SimTime_t>("txLatency",0);
 
-	SST::UnitAlgebra busBandwidth = params.find<SST::UnitAlgebra>( "busBandwidth", "15GB/s" );
-
 	m_toHostBandwidth = params.find<SST::UnitAlgebra>( "toHostBandwidth", busBandwidth ).getRoundedValue();
 	m_fromHostBandwidth = params.find<SST::UnitAlgebra>( "fromHostBandwidth", busBandwidth ).getRoundedValue();
 
-    m_selfLink = configureSelfLink("Nic::selfLink", "1 ns",
-       new Event::Handler<NicSubComponent>(this,&NicSubComponent::handleSelfEvent));
-    assert( m_selfLink );
+	m_selfLink = configureSelfLink("Nic::selfLink", "1 ns",
+		new Event::Handler<NicSubComponent>(this,&NicSubComponent::handleSelfEvent));
+	assert( m_selfLink );
 
-    if ( m_clockRate.getRoundedValue() ) {
-        m_clockHandler = new Clock::Handler<NicSubComponent>(this,&NicSubComponent::clockHandler);
-         m_timeConverter = registerClock( m_clockRate, m_clockHandler );
-        m_clocking = true;
-     }
+	m_dbg.verbose(CALL_INFO,1,1,"clockRate=%s\n", m_clockRate.toString().c_str());
+	if ( m_clockRate.getRoundedValue() ) {
+		m_clockHandler = new Clock::Handler<NicSubComponent>(this,&NicSubComponent::clockHandler);
+		m_timeConverter = registerClock( m_clockRate, m_clockHandler );
+		m_clocking = true;
+	}
 }
 
 void NicSubComponent::stopClocking( Cycle_t cycle ) {
 	assert( m_clocking == true );
 
-	m_dbg.debug(CALL_INFO,1,2,"stop clocking cycle %" PRIu64 "\n",cycle);
+	m_dbg.debug(CALL_INFO,2,2,"stop clocking cycle %" PRIu64 " %s\n",cycle, 
+			getNetworkLink().requestToReceive( m_vc ) ? "pkt avail":"no pkt" );
+
 	m_nic->enableNetNotifier();
 	m_clocking = false;
 	m_stopCycle = cycle;
@@ -60,5 +61,6 @@ void NicSubComponent::startClocking() {
 	m_nic->disableNetNotifier();
 	Cycle_t cycle = _startClocking();
 	m_totalIdleCycles += getNextClockCycle(m_timeConverter) - m_stopCycle;
-	m_dbg.debug(CALL_INFO,1,2,"start clocking cycle %" PRIu64 "\n",cycle);
+
+	m_dbg.debug(CALL_INFO,2,2,"start clocking cycle %" PRIu64 "\n",cycle);
 }
