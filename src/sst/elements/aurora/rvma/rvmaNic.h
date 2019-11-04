edb4374551bbd89e9d08783d6a20a00642dc7745
Michael J. Levenhagen <mjleven@sst-devel-new.srn.sandia.gov>
2019-11-04 09:21:31 -0700

    add concept of Recv Stream to incoming group of packets add handle to Put command and clear if needed

--- src/sst/elements/aurora/rvma/rvmaNic.h
+++ src/sst/elements/aurora/rvma/rvmaNic.h
@@ -54,27 +54,94 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 	void handleEvent( int core, Event* event );
     bool clockHandler( Cycle_t );
 
-	struct Buffer {
-		Buffer ( Hermes::MemAddr addr, size_t size, Hermes::RVMA::Completion* completion) : addr(addr), size(size), completion(completion), nBytes(0), filled(false) {}
+	class Buffer {
+	  public:
+		Buffer ( Hermes::RVMA::EpochType type, size_t threshold, Hermes::MemAddr addr, size_t size, Hermes::RVMA::Completion* completion ) : 
+			m_epochType(type), epochThreshold(threshold), addr(addr), maxSize(size), completion(completion), bytesRcvd(0), epochCount(0)
+		{
+			printf("%p\n",this);
+			//printf("%s() this=%p maxSize=%zu completion=%p\n",__func__,this,size,completion);
+		}
+
 		void copy( void* ptr, size_t offset, size_t length ) {
-			assert( offset + length <= size );
+			//printf("%s() this=%p offset=%zu length=%zu maxSize=%zu\n",__func__,this,offset,length,maxSize);
+			if( offset + length > maxSize ) {
+				printf("offset-%zu length=%zu maxSize=%zu\n",offset,length,maxSize);
+				assert(0);
+			}
 			if ( addr.getBacking() ) {
 				memcpy( addr.getBacking(offset), ptr, length);
 			}
-			nBytes += length;
+			bytesRcvd += length;
+
+			if ( isEndOfEpoch() ) {
+				endEpoch();
+			}
 		}
+
+		void incEpochCount( size_t length ) {
+			if ( m_epochType == Hermes::RVMA::Op ) {
+				++epochCount;
+			} else {
+				epochCount += length;
+			}
+			//printf("%s() epochCount=%zu\n",__func__,epochCount);
+		}
+
+		bool isEndOfEpoch() {
+			return epochThreshold == epochCount;
+		}
+
+		void endEpoch() {
+			getCompletion()->count = epochCount;
+			getCompletion()->addr = getAddr();
+		}
+
+		size_t getEpochCount() { return epochCount; }
+		Hermes::RVMA::Completion* getCompletion() { return completion; }
+		size_t getBytesRcvd() { return bytesRcvd; }
+		Hermes::MemAddr& getAddr() { return addr; }
+
+	  private:
+		Hermes::RVMA::EpochType m_epochType;
+		size_t epochThreshold;
+		size_t epochCount;
+
 		Hermes::MemAddr addr;
-		size_t size;
 		Hermes::RVMA::Completion* completion;
-		size_t nBytes;
-		bool filled;
+		size_t maxSize;
+		size_t bytesRcvd;
 	};
 
+	class RecvStream {
+	  public:
+		RecvStream( int window, Buffer* buffer, size_t length ) : m_window(window),
+				m_buffer(buffer), m_length(length), m_numRcvdPkts(0), m_numTotalPkts(0) {}
+		void setNumPkts( int num ) { m_numTotalPkts = num; }
+		void recv( void* ptr, size_t offset, size_t length ) {
+			m_buffer->copy( ptr, offset, length ); 
+		}
+		Buffer& buffer() { return *m_buffer; }
+		int getWindowNum() { return m_window; }
+		int incRcvdPkts() { ++ m_numRcvdPkts; }
+		bool rcvdAllPkts() { return m_numTotalPkts == m_numRcvdPkts; }
+	  private:
+		int m_window;
+		size_t m_length;
+		int m_numRcvdPkts;
+		int m_numTotalPkts;
+		Buffer* m_buffer;
+	};
 
 	class SendEntry {
 	  public:
-		SendEntry( int srcCore, PutCmd* cmd ) : srcCore(srcCore), cmd(cmd), currentOffset(0), completedDMAs(0)  { } 
-		~SendEntry() { delete cmd; }
+		SendEntry(int id, int srcCore, PutCmd* cmd ) : srcCore(srcCore), cmd(cmd), currentOffset(0), completedDMAs(0), streamId(id) { } 
+		~SendEntry() {
+			if ( cmd->handle ) {
+				*cmd->handle = 1;
+			}
+			delete cmd;
+		}
 
 		void decRemainingBytes( int num ) { currentOffset += num; }
 		int remainingBytes() { return cmd->size - currentOffset; }
@@ -87,6 +154,7 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 		int getSrcCore() { return srcCore; }
 	  	PutCmd& getCmd() { return *cmd; }
 		bool isDOne() { return lastPkt; }
+		int getStreamId() { return streamId; }
 
 	  private:
 		int completedDMAs;
@@ -94,6 +162,7 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 	  	PutCmd* cmd;
 		size_t currentOffset;
 		bool lastPkt;
+		int streamId;
 	};
 
 	class SelfEvent : public Event {
@@ -105,11 +174,11 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 
 
 		SelfEvent( NetworkPkt* pkt ) : Event(), type(RxLatency), pkt(pkt) {}
-		SelfEvent( int pid, int window, NetworkPkt* pkt, size_t length, uint64_t rvmaOffset  ) : 
-			Event(), type(RxDmaDone), pid(pid), window(window), pkt(pkt), length(length), rvmaOffset(rvmaOffset) {}
+		SelfEvent( int pid, RecvStream* stream, NetworkPkt* pkt, size_t length, uint64_t rvmaOffset  ) :
+			Event(), type(RxDmaDone), pid(pid), stream(stream), pkt(pkt), length(length), rvmaOffset(rvmaOffset) {}
 		NetworkPkt* pkt;
 		int pid;
-		int window;
+		RecvStream* stream;
 		size_t length;
 		uint64_t rvmaOffset;
 		SendEntry* sendEntry;
@@ -126,7 +195,6 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 			m_addr = addr;
 			m_threshold = threshold;
 			m_epochType = type;
-			m_count = 0;
 			m_oneTime = oneTime; 
 		}
 
@@ -152,33 +220,26 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 			m_active = false;
 		}
 
-		Buffer* recv( size_t offset, void* ptr, size_t nbytes ) {
-
+		Buffer* getActiveBuffer( size_t length ) {
 			if ( m_availBuffers.empty() ) {
 				return NULL;
 			}
 
 			Buffer* buffer = m_availBuffers.front();
 
-			buffer->copy( ptr, offset, nbytes );
-		
-			if ( m_epochType == Hermes::RVMA::Op ) {
-				++m_count;
-			} else {
-				m_count += nbytes;
+			// note we inc the epoch count here so any new active buffer requests go to the next buffer
+			buffer->incEpochCount(length);
+
+			if ( buffer->isEndOfEpoch() ) {
+				switchActiveBuffer();
 			}
 
-			if ( m_count == m_threshold ) {
-				endEpoch( buffer );
-			}
 			return buffer;
 		}
 
-		void endEpoch( Buffer* buffer ) {
-			buffer->filled = true;
-			buffer->completion->count = m_count;	
-			buffer->completion->addr = buffer->addr;	
+		void switchActiveBuffer( ) {
 
+			// if one time buffer this window will go away so we don't need to switch to next active buffer 
 			if ( isOneTime() ) {
 				return;
 			}
@@ -187,9 +248,8 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 				m_usedBuffers.pop_front();
 			}
 
-			m_usedBuffers.push_back( buffer );
+			m_usedBuffers.push_back( m_availBuffers.front() );
 			m_availBuffers.pop_front();
-			m_count = 0;
 		}
 
 		int getEpoch( int* epoch ) { 
@@ -204,16 +264,18 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 			if ( m_availBuffers.empty() ) {
                 return -1;
             }
-			endEpoch( m_availBuffers.front() ); 
+			m_availBuffers.front()->endEpoch();
+			switchActiveBuffer();
 			return 0; 
 		}
 
 		void getBufPtrs( std::vector<Hermes::RVMA::Completion>& vec, int max ) {
 			std::deque<Buffer*>::iterator iter = m_usedBuffers.begin();
 			for ( int i = 0; iter != m_usedBuffers.end() &&  i < max; i++, ++iter ) {
-				vec.push_back( *(*iter)->completion );
+				vec.push_back( *(*iter)->getCompletion() );
 			}	
 		}
+
 		int numAvailBuffers() { return m_availBuffers.size(); }
 		int numCompBuffers() { return m_usedBuffers.size(); }
 
@@ -224,20 +286,19 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 			}
 
 			std::deque<Buffer*>::iterator iter;
-
 			for ( iter = m_usedBuffers.begin(); iter != m_usedBuffers.end(); ++iter ) {
-				if ( (*iter)->addr.getSimVAddr() == addr.getSimVAddr() ) {
+				if ( (*iter)->getAddr().getSimVAddr() == addr.getSimVAddr() ) {
 					m_usedBuffers.erase(iter);
-					m_availBuffers.push_back( new Buffer( addr, size, completion) );
+					m_availBuffers.push_back( new Buffer( m_epochType, m_threshold, addr, size, completion) );
 					return 0;
 				}
 			}
 			for ( iter = m_availBuffers.begin(); iter != m_availBuffers.end(); ++iter ) {
-				if ( (*iter)->addr.getSimVAddr() == addr.getSimVAddr() ) { 
+				if ( (*iter)->getAddr().getSimVAddr() == addr.getSimVAddr() ) { 
 					return -1;
 				}
 			}
-			m_availBuffers.push_back( new Buffer( addr, size, completion) );
+			m_availBuffers.push_back( new Buffer( m_epochType, m_threshold, addr, size, completion) );
 			return 0;
 		}
 		Hermes::RVMA::VirtAddr getWinAddr() { return m_addr; }
@@ -250,7 +311,6 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 		bool m_active;
 		Hermes::RVMA::VirtAddr m_addr;
 	   	size_t m_threshold;
-		size_t m_count;
 		bool m_oneTime;
 		Hermes::RVMA::EpochType m_epochType;
 		std::deque<Buffer*> m_availBuffers;
@@ -264,7 +324,7 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 	void processSendPktStart( NetworkPkt*, SendEntry*, size_t length, bool lastPkt );
 	void processSendPktFini( NetworkPkt*, SendEntry*, bool lastPkt );
 	void processRecvPktStart( NetworkPkt* );
-	void processRecvPktFini( int pid, int window, NetworkPkt*, size_t length, uint64_t rvmaOffset );
+	void processRecvPktFini( int pid, RecvStream*, NetworkPkt*, uint64_t rvmaOffset, size_t length );
 	void setNumCores( int num );
 	void initWindow( int coreNum, Event* event );
 	void closeWindow( int coreNum, Event* event );
@@ -285,23 +345,72 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 	void handleSelfEvent( SelfEvent* );
 	bool processSendQ( Cycle_t cycle );
 
-	struct Core {
-		Core() : m_mwaitCmd(NULL) {}
-		std::vector<Window> m_windowTbl;
-		MwaitCmd*   		m_mwaitCmd;
-		std::deque< Buffer* > m_completed;
+	class Core {
 
-		int findWindow( Hermes::RVMA::VirtAddr winAddr ) {
+	  public:
+		Core() : m_mwaitCmd(NULL) {}
+
+		RecvStream* createRecvStream( Hermes::RVMA::VirtAddr winAddr, size_t length ) {
+
 			for ( int i = 0; i < m_windowTbl.size(); i++ ) {
 				if ( m_windowTbl[i].isActive() ) {
 					if ( m_windowTbl[i].checkWinAddr( winAddr ) ) {
-						return i;
+						Buffer* buffer = m_windowTbl[i].getActiveBuffer( length );
+						assert( buffer );
+						return new RecvStream( i, buffer, length );
 					}
 				}	
 			}
-			return -1;
+			return NULL;
 		}
 
+		uint64_t genKey( int srcNid, uint16_t srcPid, StreamId streamId ) {
+			return (uint64_t) srcNid << 32 | srcPid << 16 | streamId ;
+		}
+
+		bool activeRecvStream( int srcNid, int srcPid, StreamId streamId ) {
+			uint64_t key = genKey( srcNid, srcPid, streamId );
+			return m_activeStreams.find(key) != m_activeStreams.end();
+		}
+
+		void setActiveRecvStream( int srcNid, int srcPid, StreamId streamId, RecvStream* buf ) {
+			assert( ! activeRecvStream( srcNid, srcPid, streamId ) );
+			uint64_t key = genKey( srcNid, srcPid, streamId );
+			m_activeStreams[key] = buf;
+		}
+
+		void clearActiveRecvStream( int srcNid, int srcPid, StreamId streamId ) {
+			assert( activeRecvStream( srcNid, srcPid, streamId ) );
+			uint64_t key = genKey( srcNid, srcPid, streamId );
+			m_activeStreams.erase(key);
+		}
+
+		RecvStream* findActiveRecvStream( int srcNid, int srcPid, StreamId streamId ) {
+			uint64_t key = genKey( srcNid, srcPid, streamId );
+
+			streamMap_t::iterator iter = m_activeStreams.find(key);
+
+			RecvStream* buf = NULL;
+			if ( iter != m_activeStreams.end() ) {
+				buf = iter->second;
+			}
+
+			return buf;
+		}
+
+		size_t getWindowTblSize() { return m_windowTbl.size(); }
+		void windowTblResize(int num) { return m_windowTbl.resize(num); }
+		void setWaitCmd(MwaitCmd* cmd) { m_mwaitCmd = cmd; }
+		void clearWaitCmd() { delete m_mwaitCmd; m_mwaitCmd = NULL; } 
+		Window& getWindow(int num) { return m_windowTbl[num]; }
+		MwaitCmd* getWaitCmd() { return m_mwaitCmd; }
+
+	  private:
+		std::vector<Window> m_windowTbl;
+		MwaitCmd*   		m_mwaitCmd;
+		std::deque< Buffer* > m_completed;
+		typedef std::map<uint64_t,RecvStream*>  streamMap_t;
+		streamMap_t m_activeStreams;
 	};
 
 	std::vector<Core>   m_coreTbl;
@@ -327,6 +436,9 @@ class RvmaNicSubComponent : public Aurora::NicSubComponent {
 
 	std::queue<SelfEvent*> m_selfEventQ;
 	SimpleNetwork::Request* m_pendingNetReq;
+	StreamId m_streamIdCnt;
+	std::vector<uint64_t> m_curSentPktNum;
+	std::vector<uint64_t> m_curRecvPktNum;
 };
 
 }
